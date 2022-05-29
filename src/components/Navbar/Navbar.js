c08e8649307be0bb5b3412fdcda8316793d564f0
fahimshahriar786 <fahimshahariar786@gmail.com>
2022-05-29 19:47:57 +0600

    navbar hader

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -50,7 +50,64 @@ const Navbar = ({ bg, textColor, width }) => {
   window.addEventListener("scroll", handleChangeHeader);
 
   return (
-   
+    <header
+    className={`${
+      changeHeader ? "bg-gray-600" : `${bg}`
+    } w-full  fixed top-0 left-0  z-50 transition duration-200`}
+  >
+    {/* desktop nav  */}
+    <nav
+      className={`flex items-center px-12 py-3 font-primary ${width} mx-auto`}
+    >
+      {/* brand  */}
+      <div className="flex items-center flex-grow">
+        <Link to="/" className="flex items-center space-x-2">
+          {changeHeader ? (
+            <img
+              src="../../../assets/logo.png"
+              alt="logo"
+              className="w-14 h-14"
+            />
+            
+          ) : (
+            <img
+              src="../../../assets/logo.png"
+              alt="logo"
+              className="w-14 h-14"
+            />
+          )}
+          {changeHeader ? (
+          <span className="text-xl md:text-xl lg:text-2xl font-logo font-bold text-black text-center">
+           Time Zone
+        </span>) : (
+          <span className="text-xl md:text-xl lg:text-2xl font-logo font-bold text-blue-200 text-center">
+           Time Zone
+        </span>
+          )}
+        </Link>
+       
+        {/* <button onClick={handleBlog}>Blogs</button> */}
+        <button className="btn-primary-2 text-black px-3 mx-5" onClick={handlehome}>
+                Home
+                <Link to="/Home"></Link>
+              </button>
+
+
+        <button className="btn-primary-2 text-black px-3 mx-5" onClick={handleBlog}>
+                Blogs
+                <Link to="/blogs"></Link>
+              </button>
+
+
+        <button className="btn-primary-2 text-black px-3" onClick={handlePortfolio}>
+                Portfolio
+                <Link to="/Portfolio"></Link>
+              </button>
+
+
+        
+      </div>
+
   );
 };
 
