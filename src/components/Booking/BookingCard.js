90fc4e6ce436e4c3806763e8dcfad1b947c046b7
fahimshahriar786 <fahimshahariar786@gmail.com>
2022-05-29 19:24:05 +0600

    booking card section

--- src/components/Booking/BookingCard.js
+++ src/components/Booking/BookingCard.js
@@ -6,7 +6,22 @@ const BookingCard = (props) => {
   const { title, description, image, duration, groupMembers, price, country } =
     props;
   return (
-    
+    <>
+      <div className="py-4 box-border overflow-hidden relative flex flex-col space-y-3">
+        <div>
+          {/* country badge  */}
+          <div className="absolute top-10 text-sm left-0 z-50 bg-yellow-400 font-primary px-4 py-1 rounded-lg">
+            {country}
+          </div>
+          {/* image  */}
+          <div className="overflow-hidden rounded-xl h-full w-full">
+            <img
+              className="transform hover:scale-125 transition duration-700 w-full h-full object-cover"
+              src={image}
+              alt={title}
+            />
+          </div>
+        </div>
   );
 };
 
