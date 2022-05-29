7bf0cf008d35c6582786d6c1761efcd357fccba3
fahimshahriar786 <fahimshahariar786@gmail.com>
2022-05-29 19:21:57 +0600

    about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -21,6 +21,19 @@ const About = () => {
   ];
   return (
     
+<div className="bg-red-100 py-10">
+      <section className="max-w-screen-xl  mx-auto px-6">
+      {/* heading  */}
+      <Fade left>
+        <div className="flex justify-center items-center flex-col">
+          <h1 className="font-logo text-gray-800 text-3xl font-semibold">
+             Time Zone Query !
+          </h1>
+          <div className="h-1 w-28 bg-gray-400 rounded-full"></div>
+        </div>
+      </Fade>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 my-8">
+        {/* left side image  */}
   );
 };
 
