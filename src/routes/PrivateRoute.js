bd69bf176c0f8c32b99ffb25f487162d0b01c7f0
fahimshahriar786 <fahimshahariar786@gmail.com>
2022-05-29 20:06:55 +0600

    private route fix

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -1,1 +1,19 @@
 
+import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
+
+import useAuth from "../hooks/useAuth";
+
+
+
+const PrivateRoute = ({ children, ...rest }) => {
+  const location = useLocation();
+  const { user } = useAuth();
+  if(!user.displayName){
+    return <Navigate to="/register" state={{from:location}} replace/>;
+  }
+  return children;
+  
+};
+
+export default PrivateRoute;
